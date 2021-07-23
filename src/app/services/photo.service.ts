964822ad5679b16ab2b87b12a9c4ef30ed839337
Matt Klepeis <mattklepeis@gmail.com>
2021-07-22 22:28:53 -0400

    Update app

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,19 @@
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+
+export class PhotoService {
+
+  public photos: Photo[] = [];
+
+  constructor() { }
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+  }
+}
